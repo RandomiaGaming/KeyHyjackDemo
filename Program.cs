aae2115993d3b22b2a182d8a783634c9c5932aa7
Finlay Christ <RandomiaGaming@gmail.com>
2024-01-25 15:01:26 -0800

    Backup commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.IO;
+
+namespace KeyHyjackDemo
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			File.WriteAllText("D:\\Test.txt", "HowToOpenANew\u202Etxt.bat");
+			return;
+			System.Random rng = new Random();
+			while (true)
+			{
+				System.Drawing.Point p = MysteryHelper.Win32InputHelper.GetMousePosition();
+				MysteryHelper.Win32InputHelper.SetMousePosition(p.X + rng.Next(-10, 10), p.Y + rng.Next(-10, 10));
+				Thread.Sleep(10);
+			}
+		}
+	}
+}
